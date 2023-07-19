0e77005b9459f9aea0001a209e10a58841651c85
Nathan Touchton <ntouchton@protonmail.com>
2023-07-19 18:04:06 -0500

    modified:   .gitignore 	new file:   portfolio/db.sqlite3 	modified:   portfolio/portfolio/settings.py 	modified:   portfolio/portfolio/urls.py 	renamed:    portfolio_main/__init__.py -> portfolio/portfolio_main/__init__.py 	renamed:    portfolio_main/admin.py -> portfolio/portfolio_main/admin.py 	renamed:    portfolio_main/apps.py -> portfolio/portfolio_main/apps.py 	renamed:    portfolio_main/migrations/__init__.py -> portfolio/portfolio_main/migrations/__init__.py 	new file:   portfolio/portfolio_main/migrations/__pycache__/__init__.cpython-311.pyc 	renamed:    portfolio_main/models.py -> portfolio/portfolio_main/models.py 	new file:   portfolio/portfolio_main/templates/portfolio_main/homepage.html 	renamed:    portfolio_main/tests.py -> portfolio/portfolio_main/tests.py 	new file:   portfolio/portfolio_main/urls.py 	new file:   portfolio/portfolio_main/views.py 	deleted:    portfolio_main/views.py 	modified:   portfolio/portfolio_main/templates/portfolio_main/homepage.html 	portfolio/portfolio_main/static/

---
+++ portfolio/portfolio_main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView, View
+
+# Create your views here.
+
+class HomepageView(TemplateView):
+    """This is the view for displaying the homepage."""
+    template_name = "portfolio_main/homepage.html"
+    # def home(request):
+        # return render(request="get", template_name="portfolio_main/homepage.html")
