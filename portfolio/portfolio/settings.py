ec6bdf3659ac96e43ffc206d7c550e2b18dabc0c
Nathan Touchton <ntouchton@protonmail.com>
2023-07-25 22:20:38 -0500

    modified:   portfolio/portfolio/settings.py 	modified:   portfolio/portfolio_main/models.py 	modified:   portfolio/portfolio_main/urls.py 	modified:   portfolio/portfolio_main/views.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5l_8&8n4mndf-$bzr8e4om324*pl0%k7s1#4kzanbaqn!zy3z!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["172.232.14.10"]
+ALLOWED_HOSTS = ["172.232.14.10", "0.0.0.0"]
 
 # Application definition
 
