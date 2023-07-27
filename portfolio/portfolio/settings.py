cdf4b2943993a2855edbf7f91a55adc1afcfb408
Nathan Touchton <ntouchton@protonmail.com>
2023-07-27 16:55:28 -0500

    modified:   portfolio/portfolio/settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["ntprogramming.us", "www.ntprogramming.us"]
+ALLOWED_HOSTS = ["ntprogramming.us", "www.ntprogramming.us", "172.232.14.10"]
 
 # Application definition
 
