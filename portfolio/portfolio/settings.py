d2f5b4687f9cdd8b0bf1c710a0d356aace66b597
Nathan Touchton <ntouchton@protonmail.com>
2023-07-28 14:00:17 -0500

    modified:   portfolio/portfolio/settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -131,3 +131,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 EMAIL_PORT = 587
+
+ADMINS = [("Nathan, ntouchton@protonmail.com")]
