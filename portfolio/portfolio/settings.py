06e616d54a5d5b58ce13d895c2f5592bc121176c
Nathan Touchton <ntouchton@protonmail.com>
2023-07-28 17:22:09 -0500

    modified:   .gitignore 	modified:   portfolio/portfolio/settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(find_dotenv())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
