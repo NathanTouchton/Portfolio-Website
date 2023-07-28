96f65f434930ebd56235ad83d2e2f49aa4834318
Nathan Touchton <ntouchton@protonmail.com>
2023-07-28 14:49:52 -0500

    modified:   portfolio/portfolio/settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["ntprogramming.us", "www.ntprogramming.us", "172.232.14.10", "127.0.0.1"]
 
