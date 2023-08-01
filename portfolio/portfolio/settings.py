2d56a6495c906d5983320aa107c9e7f1261b53da
Nathan Touchton <ntouchton@protonmail.com>
2023-08-01 09:39:48 -0500

    modified:   portfolio/portfolio/settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -134,6 +134,6 @@ EMAIL_HOST = "smtp.mail.yahoo.com"
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
-EMAIL_PORT = 2525
+EMAIL_PORT = 587
 
 ADMINS = [("Nathan, ntouchton@protonmail.com")]
